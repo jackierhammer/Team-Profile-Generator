a1c8f641cbfbd100915a52cf620d93352513b500
Jackie <jackierhammer@gmail.com>
2022-08-26 01:30:16 -0600

    resolved issue where prompts ran simultaneously

--- index.js
+++ index.js
@@ -11,30 +11,77 @@ const Manager = require("./lib/Manager.js");
 
 let employeeArray = [];
 
-const createManager = function () {
-    inquirer
-    .prompt([
-        {
-        type: 'input',
-        name: 'managerName',
-        message: 'Please tell me the name of your team manager.',
-        },
-        {
-        type: 'input',
-        name: 'managerID',
-        message: 'Please type the employee id of your team manager.',
-        },
-        {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'Please type the email of your team manager.',
-        },
-        {
-        type: 'input',
-        name: 'managerOffice',
-        message: 'Please type the office number of your team manager.',
-        },
-    ])
+const managerQuestions = [
+    {
+    type: 'input',
+    name: 'managerName',
+    message: 'Please tell me the name of your team manager.',
+    },
+    {
+    type: 'input',
+    name: 'managerID',
+    message: 'Please type the employee id of your team manager.',
+    },
+    {
+    type: 'input',
+    name: 'managerEmail',
+    message: 'Please type the email of your team manager.',
+    },
+    {
+    type: 'input',
+    name: 'managerOffice',
+    message: 'Please type the office number of your team manager.',
+    },
+];
+
+const engineerQuestions = [
+    {
+    type: 'input',
+    name: 'engineerName',
+    message: 'Please tell me the name of this engineer.',
+    },
+    {
+    type: 'input',
+    name: 'engineerID',
+    message: 'Please type the employee id of this engineer.',
+    },
+    {
+    type: 'input',
+    name: 'engineerEmail',
+    message: 'Please type the email of this engineer.',
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'Please type the github username of this engineer.',
+    },
+];
+
+const internQuestions = [
+    {
+    type: 'input',
+    name: 'internName',
+    message: 'Please tell me the name of this intern.',
+    },
+    {
+    type: 'input',
+    name: 'internID',
+    message: 'Please type the employee id of this intern.',
+    },
+    {
+    type: 'input',
+    name: 'internEmail',
+    message: 'Please type the email of this intern.',
+    },
+    {
+    type: 'input',
+    name: 'school',
+    message: 'Please type the school of this intern.',
+    },
+];
+
+async function main() {
+    const manager = await inquirer.prompt(managerQuestions)
     .then((data) => {
         const teamManager = new Manager(
             `${data.managerName}`, 
@@ -43,12 +90,24 @@ const createManager = function () {
             parseInt(`${data.managerOffice}`)
         );
         employeeArray.push(teamManager);
-        console.log(employeeArray);
+    });
+    const engineer = await inquirer.prompt(engineerQuestions)
+    .then((data) => {
+        const newEngineer = new Engineer(
+            `${data.engineerName}`, 
+            parseInt(`${data.engineerID}`), 
+            `${data.engineerEmail}`, 
+            `${data.github}`
+        );
+        employeeArray.push(newEngineer);
+        console.log(typeof employeeArray[0]);
     });
 };
-createManager();
+
+main();
 
 
 // fs.writeFile('./dist/index.html', template(data), (err) =>
 //   err ? console.log(err) : console.log('Your webpage has been generated.')
 // );
+
